329ca9a890f32e9802777dbbc7e1be18a4c8c2ad
Hikaru-j3339 <j3339pie@sit.ac.jp>
2025-11-26 14:49:57 +0900

    動作確認&不正な入力に対する挙動確認

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 #SPDX-FileCopyrightText: 2025 Shioiri Hikaru
 #SPDX-License-Identifier: BSD-3-Clause
 
@@ -10,7 +10,15 @@ ng() {
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 14 ] || ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}
+
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
